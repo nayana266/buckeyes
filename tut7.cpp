30f0df2a5b617b2b41fc8185c796bff878105ad7
nayana266 <66182626+nayana266@users.noreply.github.com>
2021-12-21 08:36:28 -0700

    Add files via upload

---
+++ tut7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 8 - 2;
+    cout << x << endl;
+
+    int y = 8 * 2;
+    cout << y << endl;
+
+    int z = 80 / 2;
+    cout << z << endl;
+
+    int a = 24 % 7;
+    cout << a << endl;
+
+    int b = (4 + 6) * 3;
+    cout << b;
+    return 0;
+}
