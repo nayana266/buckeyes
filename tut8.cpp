5cc4c146949122eacd1bdaecdb5c800c498330c0
nayana266 <66182626+nayana266@users.noreply.github.com>
2021-12-21 08:46:20 -0700

    Add files via upload

---
+++ tut8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    if (3>=3){
+        cout << "I love donuts!!";
+    }
+
+    if (1<=3){
+        cout << "I love donuts!!";
+    }
+
+    if (3==3){
+        cout << "I love donuts!!";
+    }
+
+    if (2!=3){
+        cout << "I love donuts!!";
+    }
+    return 0;
+}
