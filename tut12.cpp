5b10c13d744226f7f72377aed80ff54a9a7d4bbf
nayana266 <66182626+nayana266@users.noreply.github.com>
2021-12-22 12:38:31 -0700

    Add files via upload

---
+++ tut12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class NayanasClass{
+public:
+    void coolSaying(){
+    cout << "preachin to the choir" << endl;
+    }
+
+};
+int main()
+{
+    NayanasClass nayanasObject;
+    nayanasObject.coolSaying();
+    return 0;
+}
