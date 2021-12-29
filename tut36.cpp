1979a72f20e7c2e5f1b6905f10268f88c130bafb
nayana266 <66182626+nayana266@users.noreply.github.com>
2021-12-29 14:08:07 -0700

    Add files via upload

---
+++ tut36.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int donut[2][3] = {{2,3,4},{8,9,10}};
+    // 2 3 4
+    // 8 9 10
+
+    cout << donut[0][2];
+}
+
---
+++ tut38.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int fish = 5;
+    cout << &fish << endl;
+
+    int *fishPointer;
+    fishPointer = &fish;
+
+    cout << fishPointer << endl;
+
+}
